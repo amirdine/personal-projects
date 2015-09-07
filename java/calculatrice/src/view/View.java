a091bc9fd318566fa83c4b6341ce5e6d4496bec9
Amirdine MDJASSIRI <mdjassiri.amirdine@yahoo.fr>
2015-09-07 08:19:15 +0200

    ajout code source java calculatrice

---
+++ java/calculatrice/src/view/View.java
@@ -0,0 +1,32 @@
+package view;
+
+import view.util.CalcPane;
+import view.util.Position;
+
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+
+public class View {
+	
+	
+	public Scene getScene(){
+		
+		Group root = new Group();
+		Scene scene = new Scene(root, 252, 312, Color.WHITE);
+		
+		KeypadView keypadView = new KeypadView();
+		ScreenView screenView = new ScreenView();
+				
+		CalcPane calcPane = new CalcPane();
+		
+		calcPane.add(keypadView.getKeyPad(), new Position(60,4));
+		calcPane.add(screenView.getScreen(), new Position(20,15));
+		
+		root.getChildren().add(calcPane.getContent());
+		
+		return scene;
+		
+	}
+	
+}
